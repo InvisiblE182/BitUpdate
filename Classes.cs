4256c924dafc18a910d83836d37feccebd3db8ed
InvisiblE182 <osobnimail@protonmail.com>
2017-05-30 23:29:39 +0200

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BitUpdate
+{
+    public class Time
+    {
+        public string updated { get; set; }
+        public string updatedISO { get; set; }
+        public string updateduk { get; set; }
+    }
+
+    public class USD
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class GBP
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class Bpi
+    {
+        public USD USD { get; set; }
+        public GBP GBP { get; set; }
+        public EUR EUR { get; set; }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http.Headers;
+
+
+
+namespace BitUpdate
+{
+
+    public class UnformatedBitDetail
+    {
+        public Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public Bpi bpi { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UnformatedBitDetail ubd = new UnformatedBitDetail();
+            string BitPrice = ubd.bpi.EUR.rate;
+            double Bitcoins = 0.0003;
+            double Portfolio = Convert.ToDouble(BitPrice) * Bitcoins;
+            double Investment = 270;
+            double Balance = Portfolio - Investment;
+            Console.WriteLine("Current price of bitcoin is {0}", BitPrice);
+            Console.WriteLine("Your portfolio is {0}", Portfolio);
+            Console.WriteLine("Your balance is {0} EUR {1} CZK", Balance, Balance * 27);
+            Console.ReadLine();
+        }
+
+        
+
+        public static async Task<Program> GetBitAsync()
+        {
+
+            try
+            {
+                var http = new HttpClient();
+                var response = await http.GetAsync("http://api.coindesk.com/v1/bpi/currentprice.json");
+                var result = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<Program>(result);
+                return data;
+            }
+            catch (Exception e)
+            {
+                // Throw an HttpException with customized message.
+                throw new Exception("No connection");
+            }
+
+        }
+    }
+
+}
