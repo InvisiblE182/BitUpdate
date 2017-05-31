c75479b758c9b2a4957951e05781b8b7356c70e9
InvisiblE182 <osobnimail@protonmail.com>
2017-05-31 23:13:25 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,27 +19,32 @@ namespace BitUpdate
         public string disclaimer { get; set; }
         public string chartName { get; set; }
         public Bpi bpi { get; set; }
+        
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            UnformatedBitDetail ubd = new UnformatedBitDetail();
-            string BitPrice = ubd.bpi.EUR.rate;
-            double Bitcoins = 0.0003;
-            double Portfolio = Convert.ToDouble(BitPrice) * Bitcoins;
-            double Investment = 270;
-            double Balance = Portfolio - Investment;
-            Console.WriteLine("Current price of bitcoin is {0}", BitPrice);
-            Console.WriteLine("Your portfolio is {0}", Portfolio);
-            Console.WriteLine("Your balance is {0} EUR {1} CZK", Balance, Balance * 27);
-            Console.ReadLine();
+            Task.Run(async () =>
+            {
+                UnformatedBitDetail ubd = new UnformatedBitDetail();
+                ubd = await GetBitAsync();
+                double BitPrice = ubd.bpi.EUR.rate_float;
+                double Bitcoins = 0.0003;
+                double Portfolio = BitPrice * Bitcoins;
+                double Investment = 270;
+                double Balance = Portfolio - Investment;
+                Console.WriteLine("Current price of bitcoin is {0}", BitPrice);
+                Console.WriteLine("Your portfolio is {0}", Portfolio);
+                Console.WriteLine("Your balance is {0} EUR {1} CZK", Balance, Balance * 27);
+                Console.ReadLine();
+            }).GetAwaiter().GetResult();
+            
         }
-
+    
         
-
-        public static async Task<Program> GetBitAsync()
+        public static async Task<UnformatedBitDetail> GetBitAsync()
         {
 
             try
@@ -47,7 +52,7 @@ namespace BitUpdate
                 var http = new HttpClient();
                 var response = await http.GetAsync("http://api.coindesk.com/v1/bpi/currentprice.json");
                 var result = await response.Content.ReadAsStringAsync();
-                var data = JsonConvert.DeserializeObject<Program>(result);
+                var data = JsonConvert.DeserializeObject<UnformatedBitDetail>(result);
                 return data;
             }
             catch (Exception e)
